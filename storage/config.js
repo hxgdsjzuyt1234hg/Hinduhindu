b1ea6d2f35daa6c40f849f7cc61303de7e052991
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-03-02 13:28:17 +0530

    Add files via upload

---
+++ storage/config.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [6140851230], // ganti jadi id mu
+    botToken: "7585334160:AAE2O4jUxktwMvLKXEIfRQ97SPbtbCe830U", //isi make token bot mu
+}
+
+module.exports = global;
