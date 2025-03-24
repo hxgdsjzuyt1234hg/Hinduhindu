63113e305749177a636dd7f5ca8c5a3c6ee3fea4
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-03-24 23:04:53 +0530

    Update config.js

--- storage/config.js
+++ storage/config.js
@@ -1,6 +1,6 @@
 const global = {
-    owner: [7430156268], // ganti jadi id mu
-    botToken: "7776286968:AAEv-aNlU21h8E2uEiHiAsj8mY7PEVqA2ks", //isi make token bot mu
+    owner: [7865363008], // ganti jadi id mu
+    botToken: "7585283331:AAFBTZMSJrBLhAvEYSTkS5OX93vFchNoHqw", //isi make token bot mu
 }
 
 module.exports = global;
